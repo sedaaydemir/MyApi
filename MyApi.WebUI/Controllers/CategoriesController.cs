897e2b16df84888afc79c77a57b0c79f5e9b3561
Seda Aydemir <seda.aydemir7@gmail.com>
2024-12-16 23:05:03 +0300

    WebUi Katmanı eklendi

---
+++ MyApi.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApi.WebUI.Dtos;
+using Newtonsoft.Json;
+
+
+namespace MyApi.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoriesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CategyList()
+        {
+            var client =_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7266/api/Category");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ MyApi.WebUI/Dtos/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyApi.WebUI.Dtos
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
